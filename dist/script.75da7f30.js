65e2b34f6a939346339a0fe789925b636d09dc1f
charlie <charliealonso2001@gmail.com>
2021-08-17 09:21:33 -0500

    javascript added

--- dist/script.75da7f30.js
+++ dist/script.75da7f30.js
@@ -117,79 +117,172 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"../node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
-var bundleURL = null;
-
-function getBundleURLCached() {
-  if (!bundleURL) {
-    bundleURL = getBundleURL();
+})({"script.js":[function(require,module,exports) {
+var listsContainer = document.querySelector('[data-lists]');
+var newListForm = document.querySelector('[data-new-list-form]');
+var newListInput = document.querySelector('[data-new-list-input]');
+var deleteListButton = document.querySelector('[data-delete-list-button]');
+var listDisplayContainer = document.querySelector('[data-list-display-container]');
+var listTitleElement = document.querySelector('[data-list-title]');
+var listCountElement = document.querySelector('[data-list-count]');
+var tasksContainer = document.querySelector('[data-tasks]');
+var taskTemplate = document.getElementById('task-template');
+var newTaskForm = document.querySelector('[data-new-task-form]');
+var newTaskInput = document.querySelector('[data-new-task-input]');
+var clearCompleteTasksButton = document.querySelector('[data-clear-complete-tasks-button]');
+var LOCAL_STORAGE_LIST_KEY = 'task.lists';
+var LOCAL_STORAGE_SELECCTED_LIST_ID_KEY = "task.selectedListId";
+var lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
+var selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECCTED_LIST_ID_KEY);
+listsContainer.addEventListener('click', function (e) {
+  if (e.target.tagName.toLowerCase() === 'li') {
+    selectedListId = e.target.dataset.listId;
+    saveAndRender();
   }
-
-  return bundleURL;
-}
-
-function getBundleURL() {
-  // Attempt to find the URL of the current script and use that as the base URL
-  try {
-    throw new Error();
-  } catch (err) {
-    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
-
-    if (matches) {
-      return getBaseURL(matches[0]);
-    }
+});
+tasksContainer.addEventListener('click', function (e) {
+  if (e.target.tagName.toLowerCase() === 'input') {
+    var selectedList = lists.find(function (list) {
+      return list.id === selectedListId;
+    });
+    var selectedTask = selectedList.tasks.find(function (task) {
+      return task.id === e.target.id;
+    });
+    selectedTask.complete = e.target.checked;
+    save();
+    renderTaskCount(selectedList);
   }
+});
+clearCompleteTasksButton.addEventListener('click', function (e) {
+  var selectedList = lists.find(function (list) {
+    return list.id === selectedListId;
+  });
+  selectedList.tasks = selectedList.tasks.filter(function (task) {
+    return !task.complete;
+  });
+  saveAndRender();
+});
+deleteListButton.addEventListener('click', function (e) {
+  lists = lists.filter(function (list) {
+    return list.id !== selectedListId;
+  });
+  selectedListId = null;
+  saveAndRender();
+});
+newListForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+  var listName = newListInput.value;
+  if (listName == null || listName === '') return;
+  var list = createList(listName);
+  newListInput.value = null;
+  lists.push(list);
+  saveAndRender();
+});
+newTaskForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+  var taskName = newTaskInput.value;
+  if (taskName == null || taskName === '') return;
+  var task = createTask(taskName);
+  newTaskInput.value = null;
+  var selectedList = lists.find(function (list) {
+    return list.id === selectedListId;
+  });
+  selectedList.tasks.push(task);
+  saveAndRender();
+});
 
-  return '/';
-}
-
-function getBaseURL(url) {
-  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)?\/[^/]+(?:\?.*)?$/, '$1') + '/';
-}
-
-exports.getBundleURL = getBundleURLCached;
-exports.getBaseURL = getBaseURL;
-},{}],"../node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
-var bundle = require('./bundle-url');
-
-function updateLink(link) {
-  var newLink = link.cloneNode();
-
-  newLink.onload = function () {
-    link.remove();
+function createList(name) {
+  return {
+    id: Date.now().toString(),
+    name: name,
+    tasks: [{
+      id: 'asdf',
+      name: 'test',
+      complete: true
+    }]
   };
-
-  newLink.href = link.href.split('?')[0] + '?' + Date.now();
-  link.parentNode.insertBefore(newLink, link.nextSibling);
 }
 
-var cssTimeout = null;
+function createTask(name) {
+  return {
+    id: Date.now().toString(),
+    name: name,
+    complete: false
+  };
+}
 
-function reloadCSS() {
-  if (cssTimeout) {
-    return;
+function saveAndRender() {
+  save();
+  render();
+}
+
+function save() {
+  localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
+  localStorage.setItem(LOCAL_STORAGE_SELECCTED_LIST_ID_KEY, selectedListId);
+}
+
+function render() {
+  clearElement(listsContainer);
+  renderLists();
+  var selectedList = lists.find(function (list) {
+    return list.id === selectedListId;
+  });
+
+  if (selectedListId == null) {
+    listDisplayContainer.style.display = 'none';
+  } else {
+    listDisplayContainer.style.display = '';
+    listTitleElement.innerText = selectedList.name;
+    renderTaskCount(selectedList);
+    clearElement(tasksContainer);
+    renderTasks(selectedList);
   }
+}
 
-  cssTimeout = setTimeout(function () {
-    var links = document.querySelectorAll('link[rel="stylesheet"]');
+function renderTasks(selectedList) {
+  selectedList.tasks.forEach(function (task) {
+    var taskElement = document.importNode(taskTemplate.content, true);
+    var checkbox = taskElement.querySelector('input');
+    checkbox.id = task.id;
+    checkbox.checked = task.complete;
+    var label = taskElement.querySelector('label');
+    label.htmlFor = task.id;
+    label.append(task.name);
+    tasksContainer.appendChild(taskElement);
+  });
+}
 
-    for (var i = 0; i < links.length; i++) {
-      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
-        updateLink(links[i]);
-      }
+function renderTaskCount(selectedList) {
+  var incompleteTaskCount = selectedList.tasks.filter(function (task) {
+    return !task.complete;
+  }).length;
+  var taskString = incompleteTaskCount === 1 ? "task" : "tasks";
+  listCountElement.innerText = "".concat(incompleteTaskCount, " ").concat(taskString, " remaining");
+}
+
+function renderLists() {
+  lists.forEach(function (list) {
+    var listElement = document.createElement('li');
+    listElement.dataset.listId = list.id;
+    listElement.classList.add("list-name");
+    listElement.innerText = list.name;
+
+    if (list.id === selectedListId) {
+      listElement.classList.add('active-list');
     }
 
-    cssTimeout = null;
-  }, 50);
+    listsContainer.appendChild(listElement);
+  });
 }
 
-module.exports = reloadCSS;
-},{"./bundle-url":"../node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"scss/main.scss":[function(require,module,exports) {
-var reloadCSS = require('_css_loader');
+function clearElement(element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+}
 
-module.hot.dispose(reloadCSS);
-module.hot.accept(reloadCSS);
-},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+render();
+},{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -393,5 +486,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/main.77bb5cfd.js.map
+},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","script.js"], null)
+//# sourceMappingURL=/script.75da7f30.js.map
